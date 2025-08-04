a6426c97a8a75f4e1b0737c867b65308380d5ced
swarnimduwadi <duwadiswarnim@gmail.com>
2025-08-03 23:32:01 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,12 +59,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Swarnim Duwadi";
+            $("#favicon").attr("href", "experience/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            document.title = "Portfolio | Swarnim Duwadi";
+            $("#favicon").attr("href", "experience/favicon.png");
         }
     });
 
